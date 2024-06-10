a6d3d493e5276bd8547f67094873736e80e94759
gbalki <serhatbalki97@gmail.com>
2024-06-10 13:35:51 +0300

    Initial commit

---
+++ src/main/java/com/serhatbalki/spring_boot_email_example/SpringBootEmailExampleApplication.java
@@ -0,0 +1,13 @@
+package com.serhatbalki.spring_boot_email_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serhatbalki/spring_boot_email_example/SpringBootEmailExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatbalki.spring_boot_email_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmailExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
